26775f3d9fd55e5a3a9e66deaa64fa7874ea8a3c
bcsasankh <bcsasankh@gmail.com>
2018-01-25 11:44:17 -0800

    updated libraries

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
     './src/index.js'
@@ -8,14 +10,34 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
       }
-    }]
+    ]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      ENV: JSON.stringify(process.env.ENV),
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ],
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
